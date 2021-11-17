bd9e0fafc502b1f8addc03f1ece0e7f04b240022
ChoiYoungHoon <dudgns2865@naver.com>
2021-11-18 01:23:42 +0900

    책 상세 페이지 구현(댓글 기능 탑재)

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask
 from db_connect import db
-import config
-from views import main_view, user_view
+from views import main_view, user_view, detail_view
 
 app = Flask(__name__)
 
 app.register_blueprint(main_view.bp)
 app.register_blueprint(user_view.bp)
+app.register_blueprint(detail_view.bp)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:dudgns2684@127.0.0.1:3306/book_borrow" #꼭 내 루트 비밀번호로 바꿔줘야 함!!
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
-
+app.secret_key = "dudgns2684"
 
 db.init_app(app)
 
