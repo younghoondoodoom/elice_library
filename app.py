129b2c59bf6f1112f6ef7a78d75c62a307e890e4
ChoiYoungHoon <dudgns2865@naver.com>
2021-11-26 23:32:08 +0900

    오류 개선

--- app.py
+++ app.py
@@ -13,10 +13,10 @@ app.register_blueprint(detail_view.bp)
 app.register_blueprint(turn_view.bp)
 app.register_blueprint(history_view.bp)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://younghoon:dudgns2684@localhost:3306/book_borrow?charset=utf8mb4"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = "OF6JFR5EZ8W4BQ14DRJQ263EER4YG5966E1GAMV4AGDGJ5AW5B"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://younghoon:dudgns2684@localhost:3306/book_borrow?charset=utf8mb4'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.secret_key = "OF6JFR5EZ8W4BQ14DRJQ263EER4YG5966E1GAMV4AGDGJ5AW5B"
+app.secret_key = 'OF6JFR5EZ8W4BQ14DRJQ263EER4YG5966E1GAMV4AGDGJ5AW5B'
 
 
 db.init_app(app)
